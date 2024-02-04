8b8b6535003cc2983c23ffc285a1f43f546d32a0
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2024-02-03 19:38:44 -0500

    Starter code

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 const express = require("express");
+const path = require("path");
+const youtube = require("./services/youtube-api");
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home page route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
