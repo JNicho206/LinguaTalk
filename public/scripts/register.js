5d4df724b38f52e2b48b84f56b529c5b6be9ef9b
Jonah Nichols <jonah@Jonahs-MacBook-Pro.local>
2024-02-28 14:50:40 -0500

    Reorganizing for typescript

---
+++ public/scripts/register.js
@@ -0,0 +1,26 @@
+const submit = document.getElementById("register-submit");
+const username = document.getElementById("username-div").getElementsByTagName("input")[0];
+const pwd = document.getElementById("password-div").getElementsByTagName("input")[0];
+const confirm_pwd = document.getElementById("password-div").getElementsByTagName("input")[0];
+
+function isValidUsername(username)
+{
+    if (username)
+}
+
+function isValidPassword(pwd)
+{
+    
+}
+
+function passwordsMatch(pwd, confirm_pwd)
+{
+
+}
+
+
+function registerSubmit()
+{
+
+}
+submit.addEventListener("click", registerSubmit());
--- services/headless-browser.js
+++
@@ -1,1 +0,0 @@
-// This is where a library like pupeteer can be used to scrape youtube for the transcript data.
